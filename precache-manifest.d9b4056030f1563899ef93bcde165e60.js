b9efe0af5b89880f58ca39661aa19690da522847
adib2004 <adityabapat2004@gmail.com>
2025-02-17 23:57:23 +0530

    Updates

---
+++ precache-manifest.d9b4056030f1563899ef93bcde165e60.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c80f34a623815e85bd4cc41208baeed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "40a6cd2a7f37cb03a7b2",
+    "url": "/static/css/2.1f505516.chunk.css"
+  },
+  {
+    "revision": "222dc05b7bd90afc26b7",
+    "url": "/static/css/main.2cf75415.chunk.css"
+  },
+  {
+    "revision": "40a6cd2a7f37cb03a7b2",
+    "url": "/static/js/2.f7e86435.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/static/js/2.f7e86435.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "222dc05b7bd90afc26b7",
+    "url": "/static/js/main.9dee1856.chunk.js"
+  },
+  {
+    "revision": "2b5af1ace57fe02ea267",
+    "url": "/static/js/runtime-main.89e7b19f.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
